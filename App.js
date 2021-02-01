823d48b5b9b80dae525051893bef3ccf302c7a83
majidELMEHAMMEDY <elmehammedymajid@gmail.com>
2021-02-01 12:04:47 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import WorldMap from 'react-native-world-map-svg'
+
+export default function App() {
+  return (
+    <View style={{flex:1}}>
+      <WorldMap onSelectContinent={(el) => alert(el)} color='black'  selectedColor="red" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
